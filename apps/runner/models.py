c9059e2a3e63d588a69c3fafbc59ff53027abec4
thinkAmi <dev.thinkami+git@gmail.com>
2015-09-10 22:57:43 +0900

    add delete statement sample

--- apps/runner/models.py
+++ apps/runner/models.py
@@ -24,3 +24,36 @@ class Store(models.Model):
 
     class Meta:
         db_table = 'store'
+
+class CascadeKey(models.Model):
+    name = models.CharField(max_length=100)
+
+class ProtectKey(models.Model):
+    name = models.CharField(max_length=100)
+
+class SetNullKey(models.Model):
+    name = models.CharField(max_length=100)
+
+class SetDefaultKey(models.Model):
+    name = models.CharField(max_length=100)
+
+class SetKey(models.Model):
+    name = models.CharField(max_length=100)
+
+class DoNothingKey(models.Model):
+    name = models.CharField(max_length=100)
+
+class Deletion(models.Model):
+    name = models.CharField(max_length=200)
+    cascade_row = models.ForeignKey(CascadeKey, on_delete=models.CASCADE)
+    protect_row = models.ForeignKey(ProtectKey, on_delete=models.PROTECT)
+    set_null_row = models.ForeignKey(SetNullKey,
+                                     null=True,
+                                     on_delete=models.SET_NULL)
+    set_default_row = models.ForeignKey(SetDefaultKey,
+                                        default=9,
+                                        on_delete=models.SET_DEFAULT)
+    set_key_row = models.ForeignKey(SetKey,
+                                    default=10,
+                                    on_delete=models.SET(11))
+    do_nothing_row = models.ForeignKey(DoNothingKey, on_delete=models.DO_NOTHING)
